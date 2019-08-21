3eb70a9f313cfadeb0b348acd48df85061d17023
pelzolga123 <pelzolga123@gmail.com>
2019-08-21 13:52:39 +0300

    Created microposts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_131611) do
+ActiveRecord::Schema.define(version: 2019_08_21_104126) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "userid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
